1fb23ab70832b76c4e174502405d7830961ee551
Bálint Berente <balint.berente@gmail.com>
2022-06-05 01:10:16 +0200

    Temporarily set environment URLs until another solution is found

--- packages/frontend/src/utils/configurations.ts
+++ packages/frontend/src/utils/configurations.ts
@@ -1,4 +1,4 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_URL || "http://localhost:3001";
+  process.env.REACT_APP_API_URL || "https://broqr.berente.net/api";
 export const SERIVCE_URL =
-  process.env.REACT_APP_SERVICE_URL || "http://localhost:3001";
+  process.env.REACT_APP_SERVICE_URL || "https://berente.net/ly";
