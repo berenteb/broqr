35adc68161cbcc0607f55c3367b6fbe57ddacc66
Bálint Berente <balint.berente@gmail.com>
2022-06-04 13:34:56 +0200

    Changed api url to service url in preview link

--- packages/frontend/src/utils/configurations.ts
+++ packages/frontend/src/utils/configurations.ts
@@ -1,1 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3001'
+export const API_BASE_URL =
+  process.env.REACT_APP_API_URL || "http://localhost:3001";
+export const SERIVCE_URL =
+  process.env.REACT_APP_SERVICE_URL || "http://localhost:3001";
